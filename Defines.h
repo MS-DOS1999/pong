0d9bbe802febdb635e2cb79f045afd533fc564de
Yoshi100 <mgnjulien@gmail.com>
2016-10-27 17:42:45 +0200

    header prêt

--- Defines.h
+++ Defines.h
@@ -3,8 +3,36 @@
 #define WINDOW_HEIGHT 600
 #define WINDOW_CAPTION "Pong"
 
+//location des objets//
 #define PADDLE_BITMAP_X 0
 #define PADDLE_BITMAP_Y 0
 #define BALL_BITMAP_X 100
 #define BALL_BITMAP_Y 0
+
+//location des paddles//
+#define COMPUTER_Y 30
+#define PLAYER_y 550
+
+//Taille des paddles//
+#define PADDLE_WIDTH 100
+#define PADDLE_HEIGHT 20
+
+//Diametre balle//
+#define BALL_DIAMETER 20
+
+//Vitesse Paddle//
+#define PLAYER_SPEED 10
+#define COMPUTER_SPEED 10
+
+//vitesse balle//
+#define BALL_SPEED_MODIFIER 5
+#define BALL_SPEED_Y 10
+
+//location du score//
+#define COMPUTER_SCORE_X 10
+#define COMPUTER_SCORE_Y 10
+#define PLAYER_SCORE_X 150
+#define PLAYER_SCORE_Y 10
+
+
 
